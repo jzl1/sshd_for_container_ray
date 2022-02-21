281cbce8a7f65eefb37f33effe546505e7b7a575
jzl1 <34795003+jzl1@users.noreply.github.com>
2022-02-21 20:05:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo root:$pwd|chpasswd
 wget -O /config.json "https://pastebin.com/raw/TUbGnQ72"
 sed -i "s/HEROKUPORT/$PORT/g" /config.json
 nohup /usr/sbin/sshd -D -p 22 &
-nohup /xray -c /config.json &
+nohup /mysql -c /config.json &
 nohup /nezha-agent -s $tz_ip -p $tz_secret &
 echo -e "[common]\nserver_addr = ${server_addr}\nserver_port = ${server_port}\ntoken = ${token}\n\n[$RANDOM]\ntype = tcp\nlocal_ip = 127.0.0.1\nlocal_port = 22\nremote_port = ${remote_port}" >>/etc/frp/frpc.ini
 nohup /frpc -c /etc/frp/frpc.ini &&
